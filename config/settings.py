14a0ff9cf785521174aab9998bc6e0b902914820
Graham G. Hughes <web@ggh.co>
2021-11-07 20:30:45 +0000

    Configured for Elastic Beanstalk

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['ENV'] == 'dev'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'grahamghughes-env.eba-kmvffbk8.eu-west-1.elasticbeanstalk.com'
+]
 
 
 # Application definition
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"env": os.environ['ENV']})
