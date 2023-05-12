de02cc602ccb61e308fca06911be3434b2689647
Graham G. Hughes <graham@forthstreet.com>
2023-05-12 14:39:13 +0100

    Initial layout and content

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            "serif": ["Georgia", "serif"],
+            "mono": ["JetBrains Mono", "Courier New", "monospace"]
+        },
+        extend: {},
+    },
+    plugins: [],
+}
